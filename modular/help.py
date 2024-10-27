fabeb090bdb84d61580fe337ace5083f4ad2f696
bluefloyd24 <gluhdb@gmail.com>
2024-10-28 03:36:57 +0700

    Update help.py

--- modular/help.py
+++ modular/help.py
@@ -28,7 +28,7 @@ async def _(c: nlx, m):
             prefix = await c.get_prefix(c.me.id)
             await m.reply(
                 CMD_HELP[c.get_arg(m)].__help__.format(next((p) for p in prefix))
-                + f"\n<b>© Mix-Userbot - @KynanSupport</b>",
+                + f"\n<b>© 𝐁𝘭𝘶𝘦𝘧𝘭𝘰𝘺𝘥-Userbot - @blque</b>",
                 quote=True,
             )
         else:
