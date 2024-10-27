d141e7956cec4cf88e10f4f9b644e4c504aa0434
bluefloyd24 <gluhdb@gmail.com>
2024-10-28 02:45:42 +0700

    Update config.py

--- config.py
+++ config.py
@@ -11,12 +11,12 @@ bot_token = getenv("bot_token", None)
 db_name = getenv("db_name", None)
 mongo_uri = getenv("mongo_uri", None)
 def_bahasa = getenv("def_bahasa", "toxic")
-log_pic = getenv("log_pic", "https://telegra.ph//file/43cec0ae0ded594b55247.jpg")
+log_pic = getenv("log_pic", "https://vault.pictures/p/8d8300ddb2b94708846aebc91b3b3c1d")
 heroku_api = getenv("heroku_api")
 heroku_app_name = getenv("heroku_app_name")
 upstream_repo = getenv(
     "upstream_repo",
-    "https://github.com/naya1503/Mix-Userbot",
+    "https://github.com/bluefloyd24/bfyd",
 )
 upstream_branch = getenv("upstream_branch", "final")
 git_token = getenv("git_token", None)
@@ -25,8 +25,4 @@ genius_api = getenv(
     "genius_api",
     "zhtfIphjnawHBcLFkIi-zE7tp8B9kJqY3xGnz_BlzQM9nhJJrD7csS1upSxUE0OMmiP3c7lgabJcRaB0hwViow",
 )
-# scheme = getenv("scheme", None)
-# hostname = getenv("hostname", None)
-# port = int(getenv("port", None))
-# username = getenv("username", None)
-# password = getenv("password", None)
+
