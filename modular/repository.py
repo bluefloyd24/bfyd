afc4a46c98f82ca5567dca144578a39f9c9e64cc
bluefloyd24 <gluhdb@gmail.com>
2024-10-30 18:06:51 +0700

    Update repository.py

--- modular/repository.py
+++ modular/repository.py
@@ -16,15 +16,15 @@ __help__ = get_cgr("help_repo")
 
 @ky.ubot("repo|repository")
 async def repo(c, m):
-    link = await get("https://api.github.com/repos/naya1503/Mix-Userbot/contributors")
+    link = await get("https://github.com/bluefloyd24/bfyd")
     orgnya = "".join(
         f"**{count}.** [{org['login']}]({org['html_url']})\n"
         for count, org in enumerate(link, start=1)
     )
-    msg = f"""<b>[Github](https://github.com/naya1503/Mix-Userbot) | [Group](t.me/kynansupport)
-```----------------
-| Contributors |
-----------------```
+    msg = f"""<b>[Github](https://github.com/bluefloyd24/bfyd) | [Group](t.me/proofniyeee)
+```--------------
+| devs?nah!! |
+--------------```
 {orgnya}</b>"""
     await c.send_message(
         m.chat.id, msg, reply_to_message_id=ReplyCheck(m), disable_web_page_preview=True
