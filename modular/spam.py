2d986607119eea66ec2b356ec7e4625a9fd186e0
bluefloyd24 <gluhdb@gmail.com>
2024-11-05 00:28:36 +0700

    Update spam.py

--- modular/spam.py
+++ modular/spam.py
@@ -98,7 +98,6 @@ async def _(c: nlx, m):
             break
 
     berenti = False
-
     else:
         if len(m.command) < 4:
             return await msg.edit(cgr("spm_2").format(em.gagal, m.command))
